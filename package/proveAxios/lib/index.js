eddaf1f11acc2d1c8b5feb3aaf28aac73350cf19
hooks_28 <1r56955415918@qq.com>
2022-01-31 11:39:15 +0800

    feat: add userAuth plugin

--- package/proveAxios/lib/index.js
+++ package/proveAxios/lib/index.js
@@ -1,6 +1,6 @@
 import { initializationAxios } from './instance';
 import { interceptorsResponseSuccess, interceptorsResponseFail, interceptorsRequestSuccess, interceptorsRequestFail, Module, dynamicModule, InitializeContainer, dynamicModuleErrorInstall, dynamicModuleSuccessInstall, } from './core';
-import { decisionInstaller, } from './types';
+import { decisionInstaller } from './types';
 import { priority, instanceAlias } from './constants';
 export { instanceAlias, priority, initializationAxios, interceptorsResponseSuccess, interceptorsResponseFail, interceptorsRequestSuccess, interceptorsRequestFail, Module, dynamicModule, InitializeContainer, decisionInstaller, dynamicModuleErrorInstall, dynamicModuleSuccessInstall, };
 //# sourceMappingURL=index.js.map
